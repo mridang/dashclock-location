706dabebffeba658744a6afa3668bb005e53327c
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2014-08-31 11:36:45 +0300

    Made minor fixes, lowered SDK requirements and used ACRA instead of Bugsense

--- src/com/mridang/location/LocationWidget.java
+++ src/com/mridang/location/LocationWidget.java
@@ -3,11 +3,13 @@ package com.mridang.location;
 import java.util.List;
 import java.util.Random;
 
+import org.acra.ACRA;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
@@ -17,7 +19,6 @@ import android.net.Uri;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.bugsense.trace.BugSenseHandler;
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
 import com.loopj.android.http.AsyncHttpClient;
@@ -35,7 +36,7 @@ public class LocationWidget extends DashClockExtension {
 
 		super.onCreate();
 		Log.d("LocationWidget", "Created");
-		BugSenseHandler.initAndStartSession(this, getString(R.string.bugsense));
+		ACRA.init(new AcraApplication(getApplicationContext()));
 
 	}
 
@@ -79,113 +80,119 @@ public class LocationWidget extends DashClockExtension {
 
 						}
 
-						//mgrLocation.requestLocationUpdates(provider, 0, 0, this);
+						// mgrLocation.requestLocationUpdates(provider, 0, 0,
+						// this);
 
 					}
 
 				}
 
-				if (locNewest != null ) {
+				if (locNewest != null) {
 
 					final Float fltAccuracy = locNewest.getAccuracy();
 
 					Log.d("LocationWidget", "Making the rrequest to reverse geocode the coordinates");
-					new AsyncHttpClient()
-					.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + locNewest.getLatitude() + "," + locNewest.getLongitude() + "&sensor=false",
-							new AsyncHttpResponseHandler() {
+					new AsyncHttpClient().get(
+							"http://maps.googleapis.com/maps/api/geocode/json?latlng=" + locNewest.getLatitude() + ","
+									+ locNewest.getLongitude() + "&sensor=false", new AsyncHttpResponseHandler() {
 
-						@Override
-						public void onSuccess(String strResponse) {
+								@Override
+								public void onSuccess(String strResponse) {
 
-							try {
-								
-								BugSenseHandler.addCrashExtraData("Response", strResponse);
+									try {
 
-								Log.v("LocationWidget", "Server reponded with: " + strResponse);
-								if (!strResponse.trim().isEmpty()) {
+										Log.v("LocationWidget", "Server reponded with: " + strResponse);
+										if (!strResponse.trim().isEmpty()) {
 
-									JSONObject jsoResponse = new JSONObject(strResponse);
-									if (jsoResponse.getString("status").equalsIgnoreCase("OK")) {
+											JSONObject jsoResponse = new JSONObject(strResponse);
+											if (jsoResponse.getString("status").equalsIgnoreCase("OK")) {
 
-										BugSenseHandler.sendEvent("Successful Request");
-										edtInformation.expandedBody("");
-										edtInformation.expandedTitle(getString(R.string.location_info, fltAccuracy.intValue()));
+												edtInformation.expandedBody("");
+												edtInformation.expandedTitle(getString(R.string.location_info,
+														fltAccuracy.intValue()));
 
-										JSONArray jsoResults = jsoResponse.getJSONArray("results");
-										Integer intMinimum = Integer.MAX_VALUE;
+												JSONArray jsoResults = jsoResponse.getJSONArray("results");
+												Integer intMinimum = Integer.MAX_VALUE;
 
-										for (Integer intResult = 0; intResult < jsoResults.length(); intResult++) {
+												for (Integer intResult = 0; intResult < jsoResults.length(); intResult++) {
 
-											JSONObject jsoBounds = jsoResults.getJSONObject(intResult).getJSONObject("geometry").getJSONObject("viewport");
+													JSONObject jsoBounds = jsoResults.getJSONObject(intResult)
+															.getJSONObject("geometry").getJSONObject("viewport");
 
-											float[] fltDistances = new float[1];
+													float[] fltDistances = new float[1];
 
-											Location.distanceBetween(
-													Double.parseDouble(jsoBounds.getJSONObject("northeast").getString("lat")),
-													Double.parseDouble(jsoBounds.getJSONObject("northeast").getString("lng")),
-													Double.parseDouble(jsoBounds.getJSONObject("southwest").getString("lat")),
-													Double.parseDouble(jsoBounds.getJSONObject("southwest").getString("lng")),
-													fltDistances);
+													Location.distanceBetween(Double.parseDouble(jsoBounds
+															.getJSONObject("northeast").getString("lat")), Double
+															.parseDouble(jsoBounds.getJSONObject("northeast")
+																	.getString("lng")), Double.parseDouble(jsoBounds
+															.getJSONObject("southwest").getString("lat")), Double
+															.parseDouble(jsoBounds.getJSONObject("southwest")
+																	.getString("lng")), fltDistances);
 
-											if (((int) fltDistances[0] - fltAccuracy) < intMinimum) {
+													if (((int) fltDistances[0] - fltAccuracy) < intMinimum) {
 
-												intMinimum = (int) (fltDistances[0] - fltAccuracy);
-												edtInformation.expandedBody(jsoResults.getJSONObject(intResult).getString("formatted_address"));
-												edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0")));
+														intMinimum = (int) (fltDistances[0] - fltAccuracy);
+														edtInformation.expandedBody(jsoResults.getJSONObject(intResult)
+																.getString("formatted_address"));
+														edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW, Uri
+																.parse("geo:0,0")));
 
+													}
+
+												}
+
+												edtInformation.visible(true);
+												publishUpdate(edtInformation);
+
+											} else if (jsoResponse.getString("status").equalsIgnoreCase("ZERO_RESULTS")) {
+												Log.w("LocationWidget",
+														"Google Reverse Geocoding API returned no results");
+												edtInformation.visible(false);
+												Toast.makeText(getApplicationContext(), R.string.zero_results,
+														Toast.LENGTH_SHORT).show();
+											} else if (jsoResponse.getString("status").equalsIgnoreCase(
+													"OVER_QUERY_LIMIT")) {
+												Log.w("LocationWidget",
+														"Google Reverse Geocoding API has hit the query limit");
+												edtInformation.visible(false);
+												Toast.makeText(getApplicationContext(), R.string.over_limit,
+														Toast.LENGTH_SHORT).show();
+											} else if (jsoResponse.getString("status").equalsIgnoreCase(
+													"REQUEST_DENIED")) {
+												Log.e("LocationWidget",
+														"Google Reverse Geocoding API denied the request");
+												edtInformation.visible(false);
+												throw new Exception("Google Reverse Geocoding API said invalid request");
+											} else if (jsoResponse.getString("status").equalsIgnoreCase(
+													"INVALID_REQUEST")) {
+												Log.e("LocationWidget",
+														"Google Reverse Geocoding API said invalid request");
+												edtInformation.visible(false);
+												throw new Exception("Google Reverse Geocoding API said invalid request");
+											} else {
+												Log.e("LocationWidget",
+														"Google Reverse Geocoding API encountred an error");
+												edtInformation.visible(false);
+												throw new Exception("Google Reverse Geocoding API said invalid request");
 											}
 
 										}
 
-										edtInformation.visible(true);
-										publishUpdate(edtInformation);
-
-									} else if (jsoResponse.getString("status").equalsIgnoreCase("ZERO_RESULTS")) {
-										BugSenseHandler.sendEvent("Zero Results");
-										Log.w("LocationWidget", "Google Reverse Geocoding API returned no results");
+									} catch (Exception e) {
 										edtInformation.visible(false);
-										Toast.makeText(getApplicationContext(), R.string.zero_results, Toast.LENGTH_SHORT).show();
-									} else if (jsoResponse.getString("status").equalsIgnoreCase("OVER_QUERY_LIMIT")) {
-										BugSenseHandler.sendEvent("Over Limit");
-										Log.w("LocationWidget", "Google Reverse Geocoding API has hit the query limit");
-										edtInformation.visible(false);
-										Toast.makeText(getApplicationContext(), R.string.over_limit, Toast.LENGTH_SHORT).show();
-									} else if (jsoResponse.getString("status").equalsIgnoreCase("REQUEST_DENIED")) {
-										BugSenseHandler.sendEvent("Request Denied");
-										Log.e("LocationWidget", "Google Reverse Geocoding API denied the request");
-										edtInformation.visible(false);
-										throw new Exception("Google Reverse Geocoding API said invalid request");
-									} else if (jsoResponse.getString("status").equalsIgnoreCase("INVALID_REQUEST")) {
-										BugSenseHandler.sendEvent("Invalid Request");
-										Log.e("LocationWidget", "Google Reverse Geocoding API said invalid request");
-										edtInformation.visible(false);
-										throw new Exception("Google Reverse Geocoding API said invalid request");
-									} else {
-										BugSenseHandler.sendEvent("Unknown Response");
-										Log.e("LocationWidget", "Google Reverse Geocoding API encountred an error");
-										edtInformation.visible(false);
-										throw new Exception("Google Reverse Geocoding API said invalid request");
+										Log.e("LocationWidget", "Encountered an error", e);
+										ACRA.getErrorReporter().handleSilentException(e);
 									}
 
 								}
 
-							} catch (Exception e) {
-								edtInformation.visible(false);
-								Log.e("LocationWidget", "Encountered an error", e);
-								BugSenseHandler.sendException(e);
-							} finally {
-								BugSenseHandler.clearCrashExtraData();
-							}
-
-						}
-
-					});
+							});
 
 				}
 
 			}
 
-			if (new Random().nextInt(5) == 0) {
+			if (new Random().nextInt(5) == 0 && !(0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE))) {
 
 				PackageManager mgrPackages = getApplicationContext().getPackageManager();
 
@@ -196,22 +203,26 @@ public class LocationWidget extends DashClockExtension {
 				} catch (NameNotFoundException e) {
 
 					Integer intExtensions = 0;
-				    Intent ittFilter = new Intent("com.google.android.apps.dashclock.Extension");
-				    String strPackage;
+					Intent ittFilter = new Intent("com.google.android.apps.dashclock.Extension");
+					String strPackage;
 
-				    for (ResolveInfo info : mgrPackages.queryIntentServices(ittFilter, 0)) {
+					for (ResolveInfo info : mgrPackages.queryIntentServices(ittFilter, 0)) {
 
-				    	strPackage = info.serviceInfo.applicationInfo.packageName;
-						intExtensions = intExtensions + (strPackage.startsWith("com.mridang.") ? 1 : 0); 
+						strPackage = info.serviceInfo.applicationInfo.packageName;
+						intExtensions = intExtensions + (strPackage.startsWith("com.mridang.") ? 1 : 0);
 
 					}
 
 					if (intExtensions > 1) {
 
 						edtInformation.visible(true);
-						edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://details?id=com.mridang.donate")));
+						edtInformation.clickIntent(new Intent(Intent.ACTION_VIEW).setData(Uri
+								.parse("market://details?id=com.mridang.donate")));
 						edtInformation.expandedTitle("Please consider a one time purchase to unlock.");
-						edtInformation.expandedBody("Thank you for using " + intExtensions + " extensions of mine. Click this to make a one-time purchase or use just one extension to make this disappear.");
+						edtInformation
+								.expandedBody("Thank you for using "
+										+ intExtensions
+										+ " extensions of mine. Click this to make a one-time purchase or use just one extension to make this disappear.");
 						setUpdateWhenScreenOn(true);
 
 					}
@@ -225,7 +236,7 @@ public class LocationWidget extends DashClockExtension {
 		} catch (Exception e) {
 			edtInformation.visible(false);
 			Log.e("LocationWidget", "Encountered an error", e);
-			BugSenseHandler.sendException(e);
+			ACRA.getErrorReporter().handleSilentException(e);
 		}
 
 		edtInformation.icon(R.drawable.ic_dashclock);
@@ -241,7 +252,6 @@ public class LocationWidget extends DashClockExtension {
 
 		super.onDestroy();
 		Log.d("LocationWidget", "Destroyed");
-		BugSenseHandler.closeSession(this);
 
 	}
 
